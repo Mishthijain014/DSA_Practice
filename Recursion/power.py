8ab7b73c48b389c5d0150f04b627e7502794880f
Mishthijain014 <mishthi.jain.s.125@kalvium.community>
2026-04-12 10:57:47 +0530

    power

--- Recursion/power.py
+++ Recursion/power.py
@@ -1,7 +1,34 @@
+# SOLUTION 1
+
+# def power(x,n):
+#     if(n==0):
+#         return 1
+#     else:
+#         return x*power(x,n-1)
+
+# print(power(2,3))
+
+
+
+# SOLUTION 2
+
 def power(x,n):
     if(n==0):
         return 1
+    if(n==1):
+        return n
+    if(n<0):
+        n = -n
+        x = 1/x
+        half = power(n//2)
+    
+    if n%2==2:
+        return half * half
     else:
-        return x*power(x,n-1)
+        return x*half*half
 
-print(power(2,3))
+
+
+
+
+
