b76c28821ebf86090971d0d3735c3b84c0768a4c
Mishthijain014 <mishthi.jain.s.125@kalvium.community>
2026-04-18 20:22:05 +0530

    something wrong

---
+++ Backtracking/longest palindrome.py
@@ -0,0 +1,24 @@
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+#         result = ""
+#         def is_palindrome(sub):
+#             return sub == sub[::-1]
+
+#         def backtracking(start,path):
+#             nonlocal result
+
+#             for end in range(start,len(s)):
+#                 substring = s[start:end+1]
+
+#                 if is_palindrome(substring):
+#                     path.append(substring)
+#                     backtracking(end+1,path)
+#                     path.pop()
+
+#             if path:
+#                 last = path[-1]
+#                 if len(last)>len(result):
+#                     result = last             
+
+#         backtracking(0,[])
+#         return result
