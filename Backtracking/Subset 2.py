bd41ec3f9be4854a038b8c34a49a6085e230a4c8
Mishthijain014 <mishthi.jain.s.125@kalvium.community>
2026-04-18 13:36:55 +0530

    something

--- Backtracking/Subset 2.py
+++ Backtracking/Subset 2.py
@@ -1,38 +1,21 @@
-# nums = [1, 2, 2]   
-# result = []
-
-# nums.sort()
-
-# def backtrack(index, subset):
-#     result.append(subset[:])
-
-#     for i in range(index, len(nums)):
-#         if i > index and nums[i] == nums[i - 1]:
-#             continue
-
-#         subset.append(nums[i])
-#         backtrack(i + 1, subset)
-#         subset.pop()
-
-
-# backtrack(0, [])
-# print(result)
-
-
+nums = [1, 2, 2]   
 result = []
-nums = [1,1,2]
 
-def backtracking(index,subsets):
-    result.append(subsets[:])
+nums.sort()
 
-    for i in range(index,len(nums)):
-        if i>index and nums[i]==nums[i-1]:
+def backtrack(index, subset):
+    result.append(subset[:])
+
+    for i in range(index, len(nums)):
+        if i > index and nums[i] == nums[i - 1]:
             continue
 
-        subsets.append(nums[i])
-        backtracking(i+1,subsets)
+        subset.append(nums[i])
+        backtrack(i + 1, subset)
+        subset.pop()
 
-        subsets.pop()
 
-backtracking(0,[])
+backtrack(0, [])
 print(result)
+
+
