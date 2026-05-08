2b1e93c13e7acc322e2ba51761eb67919df70dcc
Mishthijain014 <mishthi.jain.s.125@kalvium.community>
2026-05-08 15:07:40 +0530

    reverse string 2

---
+++ Two pointer/Reverse string.py
@@ -0,0 +1,13 @@
+def reverse_string(s):
+    left = 0 
+    right = len(s)-1
+
+    while left < right:
+        s[left] , s[right] = s[right] , s[left]
+
+        left += 1
+        right -= 1
+
+        
+
+print(reverse_string(["h","e","l","l","o"]))
